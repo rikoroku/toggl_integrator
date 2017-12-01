9635b7434131ca3b67d79f36a8e8f8d5c4a2275d
rikoroku <v2gats213@gmail.com>
2017-12-01 14:03:30 +0900

    add initial files that parser command option

--- lib/toggl_integrator/command.rb
+++ lib/toggl_integrator/command.rb
@@ -5,6 +5,21 @@ module TogglIntegrator
   # execute tasks for command line base
   # @author rikoroku
   class Command
+
+    def self.run argv
+      new(argv).execute
+    end
+
+    def initialize argv
+      @argv = argv
+    end
+
+    def execute
+      options = Options.parse! @argv
+
+      DB.prepare
+    end
+
   end
 
 end
