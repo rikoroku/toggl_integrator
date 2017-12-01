019f10b2b05751c5fd1cd3f9ff7ff4c3c8a59bec
rikoroku <v2gats213@gmail.com>
2017-12-01 15:37:26 +0900

    [add] case sub_command

--- lib/toggl_integrator/command.rb
+++ lib/toggl_integrator/command.rb
@@ -15,9 +15,31 @@ module TogglIntegrator
     end
 
     def execute
-      options = Options.parse! @argv
+      options     = Options.parse! @argv
+      sub_command = options.delete :command
 
       DB.prepare
+
+      case sub_command
+      when "start"
+        start_task options[:interval]
+      when "stop"
+        stop_task
+      when "status"
+        show_status
+      end
+
+    rescue => e
+      abort "Error: #{e.message}"
+    end
+
+    def start_task
+    end
+
+    def stop_task
+    end
+
+    def show_status
     end
 
   end
