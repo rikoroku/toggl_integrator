0ee377e7a7caeeba5e1241e5a7cac9b500801256
rikoroku <v2gats213@gmail.com>
2017-12-02 11:09:16 +0900

    [clean] move function of Toggl API to Toggl class

--- lib/toggl_integrator.rb
+++ lib/toggl_integrator.rb
@@ -2,6 +2,7 @@ require "toggl_integrator/version"
 require "toggl_integrator/command"
 require "toggl_integrator/db"
 require "toggl_integrator/task"
+require "toggl_integrator/toggl"
 require "logger"
 
 module TogglIntegrator
