677145a6939b884464be369ea5c46451b9a4f37b
rikoroku <v2gats213@gmail.com>
2017-12-02 11:09:16 +0900

    Move processing  toggl api in command.rb to outer file.

---
+++ lib/toggl_integrator/toggl.rb
@@ -0,0 +1,30 @@
+# config: utf-8
+
+require "togglv8"
+require "date"
+
+module TogglIntegrator
+
+  # Class Toggl for Toggl API
+  # @author rikoroku
+  class Toggl
+
+    def initialize log, task
+      @toggl_api  = TogglV8::API.new ENV["TOGGL_API_TOKEN"]
+      @user       = @toggl_api.me all=true
+      @log, @task = log, task
+    end
+
+    def save_time_entries
+      today        = Date.today
+      dates        = { :start_date => today-1, :end_date => today+1 }
+      projects     = @toggl_api.my_projects(@user).map { |i| { "id" => i["id"], "name" => i["name"] } }
+      time_entries = @toggl_api.get_time_entries dates.map { |k, v| [k, v.to_s] }.to_h
+      @task.save_tasks time_entries, dates, projects
+    rescue => e
+      @log.error "Error: #{e.message}"
+    end
+
+  end
+
+end
