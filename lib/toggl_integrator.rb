1689510d8b374f80b6a0d8fb3c279d59c9d6371a
rikoroku <v2gats213@gmail.com>
2017-12-02 09:44:33 +0900

    1. Remove use of command options. 2. Use logger instead of abort. 3. Fix create_table task. 4. Add save_tasks method to task.

--- lib/toggl_integrator.rb
+++ lib/toggl_integrator.rb
@@ -1,8 +1,8 @@
 require "toggl_integrator/version"
 require "toggl_integrator/command"
-require "toggl_integrator/command/options"
 require "toggl_integrator/db"
 require "toggl_integrator/task"
+require "logger"
 
 module TogglIntegrator
   # Your code goes here...
