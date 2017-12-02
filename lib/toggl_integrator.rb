dd7442515cf6e251eb59120783ed5ad40c0273b6
rikoroku <v2gats213@gmail.com>
2017-12-02 17:23:06 +0900

    Add function processing google calendar

--- lib/toggl_integrator.rb
+++ lib/toggl_integrator.rb
@@ -3,8 +3,9 @@ require "toggl_integrator/command"
 require "toggl_integrator/db"
 require "toggl_integrator/task"
 require "toggl_integrator/toggl"
+require "toggl_integrator/google_calendar"
 require "logger"
+require "yaml"
 
 module TogglIntegrator
-  # Your code goes here...
 end
