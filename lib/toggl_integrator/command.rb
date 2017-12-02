45d50829de472425a683308fdbd74458fee90dd8
rikoroku <v2gats213@gmail.com>
2017-12-02 11:09:16 +0900

    Move processing  toggl api in command.rb to outer file.

--- lib/toggl_integrator/command.rb
+++ lib/toggl_integrator/command.rb
@@ -1,8 +1,5 @@
 # config: utf-8
 
-require "togglv8"
-require "date"
-
 module TogglIntegrator
 
   # execute tasks for command line base
@@ -20,23 +17,10 @@ module TogglIntegrator
     end
 
     def execute
-      save_time_entries
-
+      Toggl.new(@log, @task).save_time_entries
     rescue => e
       @log.error "Error: #{e.message}"
     end
-
-    private
-
-      def save_time_entries
-        toggl_api    = TogglV8::API.new ENV["TOGGL_API_TOKEN"]
-        user         = toggl_api.me all=true
-        projects     = toggl_api.my_projects(user).map { |i| { "id" => i["id"], "name" => i["name"] } }
-        today        = Date.today
-        dates        = { :start_date => today-1, :end_date => today+1 }
-        time_entries = toggl_api.get_time_entries dates.map { |k, v| [k, v.to_s] }.to_h
-        @task.save_tasks time_entries, dates, projects
-      end
 
   end
 
