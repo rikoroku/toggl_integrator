e793a1fd39b465794ea4b19af85e753b1ddd4f85
rikoroku <rikoroku55@gmail.com>
2020-05-28 22:23:35 +0900

    [change] replace class name with TimeEntory instead of Task

--- lib/toggl_integrator.rb
+++ lib/toggl_integrator.rb
@@ -3,7 +3,7 @@
 require 'toggl_integrator/version'
 require 'toggl_integrator/command'
 require 'toggl_integrator/db'
-require 'toggl_integrator/task'
+require 'toggl_integrator/time_entory'
 require 'toggl_integrator/toggl'
 require 'toggl_integrator/google_calendar'
 require 'toggl_integrator/logging'
