ad2ce298f7bba7b8ae678c646f06644b1ccee537
rikoroku <rikoroku55@gmail.com>
2020-12-13 22:57:25 +0900

    [add] create_with method test to time_entry_spec.rb

--- spec/unit/models/time_entry_spec.rb
+++ spec/unit/models/time_entry_spec.rb
@@ -30,4 +30,25 @@ RSpec.describe 'TogglIntegrator::TimeEntry' do
       end
     end
   end
+
+  describe 'create_with' do
+    it 'should be created with args' do
+      args = {
+        time_entry: { 'id' => 1,
+                      'pid' => 1,
+                      'start' => '2000-01-01T10:00:00+00:00',
+                      'stop' => '2000-01-01T13:00:00+00:00',
+                      'description' => 'Test',
+                      'at' => '2000-01-01T13:00:00+00:00' },
+        projects: [{ 'id' => 1, 'name' => 'Project1' }, { 'id' => 2, 'name' => 'Project2' }]
+      }
+
+      TogglIntegrator::TimeEntry.create_with(args)
+
+      expect(TogglIntegrator::TimeEntry.all.size).to eq 1
+      expect(TogglIntegrator::TimeEntry.first.id).to eq args[:time_entry]['id']
+      expect(TogglIntegrator::TimeEntry.first.description).to eq args[:time_entry]['description']
+      expect(TogglIntegrator::TimeEntry.first.project_name).to eq args[:projects][0]['name']
+    end
+  end
 end
