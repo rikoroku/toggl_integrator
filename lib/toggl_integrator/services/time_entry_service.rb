69b4bbc5ae276b54c559cbb1158a712ded618410
rikoroku <rikoroku55@gmail.com>
2020-12-14 21:31:20 +0900

    [fix] wrong condition 'creatable?'

--- lib/toggl_integrator/services/time_entry_service.rb
+++ lib/toggl_integrator/services/time_entry_service.rb
@@ -7,7 +7,7 @@ module TogglIntegrator
       def fetch_and_store
         before_store
         Toggl.time_entries.each do |time_entry|
-          next if createable?(time_entry)
+          next unless can_store?(time_entry)
 
           TimeEntry.create_with({ time_entry: time_entry,
                                   projects: Toggl.my_projects })
@@ -24,9 +24,11 @@ module TogglIntegrator
         @dates = { start_date: today - 1, end_date: today + 1 }
       end
 
-      def createable?(time_entry)
-        time_entry['stop'].nil? || time_entry['pid'].nil? ||
-          TimeEntry.all.ids.include?(time_entry['id'])
+      def can_store?(time_entry)
+        return false if time_entry['stop'].nil? || time_entry['pid'].nil? ||
+                        TimeEntry.all.ids.include?(time_entry['id'])
+
+        true
       end
 
       def before_store
